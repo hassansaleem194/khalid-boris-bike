5896875a72d765659f7e3844064b79646c8ced19
Hassan Saleem <hassansaleem194@gmail.com>
2021-09-22 17:19:26 +0100

    hassan changes

--- lib/dock_class.rb
+++ lib/dock_class.rb
@@ -1,7 +1,14 @@
 class Dockingstation
     def release_bike
         return Bike.new
-
     end
     
+    def dock(bike)
+        @bike = bike
+    end
+
+    def see_bike
+        return "Bike docked: #{@bike}"
+    end
+
 end     
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,9 +8,12 @@ describe Dockingstation do
   it 'returns a bike ' do 
     new_bike = Dockingstation.new
     my_bike = new_bike.release_bike.working
-
     expect(my_bike).to eq true
-  end  
-
-
+  end
+  
+  it 'docks a bike' do
+    dock_bike = Dockingstation.new
+    bike_1 = dock_bike.dock("red bike")
+    expect(bike_1).to eq "Bike docked: red bike"
+  end
 end
